c4a0178e8637712888e85f2665dd223933577ecb
Farouq Akhdar <faroukakhdar@gmail.com>
2025-06-01 09:17:59 -0700

    Fix import and logic in generate_branch_with_reasons

--- core/generate_branch_with_reasons.py
+++ core/generate_branch_with_reasons.py
@@ -1,4 +1,4 @@
-from core.luma_engine import generate_palette
+from core.luma_engine import LumaEngine
 from core.tag_scorer import score_tags
 from core.cluster_logic import cluster_and_label_tags
 
@@ -6,5 +6,8 @@ def generate_palette_branch(prompt: str):
     tags = prompt.split()
     scored_tags = score_tags(tags)
     clustered_tags = cluster_and_label_tags(scored_tags)
-    palette_output = generate_palette(prompt, clustered_tags)
+    
+    luma = LumaEngine()
+    palette_output = luma.generate(prompt)
+    
     return palette_output
